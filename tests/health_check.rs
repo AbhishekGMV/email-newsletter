a53d86a43d07b3cb5c7cbb53d8a080d15bbccd6b
AbhishekGMV <abhishek.mvg@gmail.com>
2023-04-29 22:37:33 +0530

    Integration test for health check endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,19 @@
-use email_newsletter::main;
+use email_newsletter::run;
 
-#[test]
-fn dummy_test() {
-    main()
+
+fn spawn_app() {
+    let server = run()
+        .expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+}
+
+
+#[tokio::test]
+async fn health_check_will_return_200() {
+    spawn_app();
+    let client = reqwest::Client::new();
+    let response = client.get("http://localhost:8000/health").send().await.expect("Failed to send request");
+    assert!(response.status().is_success());
+    assert_eq!(response.content_length(), Some(0));
+
 }
