b86be49e5734e6ea91103c36bc990e9d8b9af1be
AbhishekGMV <abhishek.mvg@gmail.com>
2023-04-30 16:01:47 +0530

    POST/ subscribe endpoint and it's test cases

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,7 @@ use email_newsletter::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind the address"); 
+    let addr = listener.local_addr().unwrap();
+    println!("Listening on {}", addr);
     run(listener)?.await
 }
