186670db1c47d9dd7115425803f1cc7e03f0bf50
AbhishekGMV <abhishek.mvg@gmail.com>
2023-05-01 19:44:59 +0530

    shell script rename

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use secrecy::ExposeSecret;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    let subscriber = get_subscriber("email_newsletter".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let config = get_config().expect("Failed to read config file");
     let connection_pool = PgPool::connect(&config.database.connection_string().expose_secret())
