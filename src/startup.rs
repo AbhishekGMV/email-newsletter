bbc4108ed23def609909935e572c77b2c0d0cc05
AbhishekGMV <abhishek.mvg@gmail.com>
2023-05-01 19:28:01 +0530

    TracingLogger for request id middleware implementation

--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,14 @@
 use std::net::TcpListener;
-use actix_web::{dev::Server, HttpServer, web, App, middleware::Logger};
+use actix_web::{dev::Server, HttpServer, web, App};
 use sqlx::PgPool;
 use crate::routes::{health_check, subscribe}; 
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener :TcpListener, db_pool:PgPool) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health", web::get().to(health_check))
             .route("/subscribe", web::post().to(subscribe))
             .app_data(db_pool.clone())
