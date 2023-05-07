b0a985ac41b86a6a00b4b198530843497cbda31d
AbhishekGMV <abhishek.mvg@gmail.com>
2023-05-07 21:17:38 +0530

    Configuration restructure and code reformat

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,9 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("email_newsletter".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let config = get_config().expect("Failed to read config file");
-    let connection_pool = PgPool::connect(&config.database.connection_string().expose_secret())
-        .await
+    let connection_pool = PgPool::connect_lazy(&config.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
-    let url = format!("127.0.0.1:{}", config.app_port); 
+    let url = format!("{}:{}",config.application.host, config.application.port); 
     let listener = TcpListener::bind(url).expect("Failed to bind the address"); 
     let addr = listener.local_addr().unwrap();
     println!("Listening on {}", addr);
